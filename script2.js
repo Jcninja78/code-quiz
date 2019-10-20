3d596f3fad869a5a6873b7f1619b66d15a9dcb49
Kyle Jones <kylejonesnh@gmail.com>
2019-10-20 07:40:44 -0400

    added functions

--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+// Utilized https://www.sitepoint.com/simple-javascript-quiz/ which showed me how to write the question list.
 var myQuestions = [
     {
       question: "Commonly used data types DO NOT include:",
@@ -51,11 +52,91 @@ var myQuestions = [
       }
   ];
   
-  var score = 0;
-  var myQuestionsIndex = 0;
+// Utilized   https://codepen.io/gcarino/pen/LDgtn to assist me with writing variables.
+var score = 0;
+var myQuestionsIndex = 0;
+var lastQuestion = myQuestions.length -1;
+var selections = [];
 
-  var lead = document.querySelector(".lead");
-  var answerOne = document.querySelector(".btn btn-primary btn-lg");
-  var answerTwo = document.querySelector(".btn btn-dark btn-lg");
-  var answerThree = document.querySelector(".btn btn-danger btn-lg");
-  var answerFour = document.querySelector(".btn btn-success btn-lg");
+var lead = document.querySelector(".lead");
+var start = document.querySelector(".btn btn-warning btn-lg")
+var answerOne = document.querySelector(".btn btn-primary btn-lg");
+var answerTwo = document.querySelector(".btn btn-dark btn-lg");
+var answerThree = document.querySelector(".btn btn-danger btn-lg");
+var answerFour = document.querySelector(".btn btn-success btn-lg");
+var timer = document.querySelector("#timer");
+var counter = document.querySelector("#counter");
+var score = document.querySelector("#score");
+
+start.addEventListener("click", startQuiz);
+
+
+// Found how to perform functions on https://simplestepscode.com/javascript-quiz-tutorial/
+// and https://www.youtube.com/watch?v=jvk1pFNqXaw.
+function showQuestionOne(question, answers, correctAnswer){
+    var output = [];
+    var answers = (answerOne + answerTwo + answerThree + answerFour);
+    for(var i = 0; i < myQuestions.length; i++){
+        answers = [];
+        for(letter in myQuestions[i].answers){
+            answers.push(answerOne + answerTwo + answerThree + answerFour);
+            output.push(score);
+        }
+    }
+}
+
+function showQuestionTwo(){
+    var output = [];
+    var answers = (answerOne + answerTwo + answerThree + answerFour);
+    for(var i = 0; i < myQuestions.length; i++){
+        answers = [];
+        for(letter in myQuestions[i].answers){
+            answers.push(answerOne + answerTwo + answerThree + answerFour);
+            output.push(score);
+        }
+    }
+}
+
+function showQuestionThree(){
+    var output = [];
+    var answers = (answerOne + answerTwo + answerThree + answerFour);
+    for(var i = 0; i < myQuestions.length; i++){
+        answers = [];
+        for(letter in myQuestions[i].answers){
+            answers.push(answerOne + answerTwo + answerThree + answerFour);
+            output.push(score);
+        }
+    }
+}
+
+function showQuestionFour(){
+    var output = [];
+    var answers = (answerOne + answerTwo + answerThree + answerFour);
+    for(var i = 0; i < myQuestions.length; i++){
+        answers = [];
+        for(letter in myQuestions[i].answers){
+            answers.push(answerOne + answerTwo + answerThree + answerFour);
+            output.push(score);
+        }
+    }
+}
+
+function showQuestionFive(){
+    var output = [];
+    var answers = (answerOne + answerTwo + answerThree + answerFour);
+    for(var i = 0; i < myQuestions.length; i++){
+        answers = [];
+        for(letter in myQuestions[i].answers){
+            answers.push(answerOne + answerTwo + answerThree + answerFour);
+            output.push(score);
+        }
+    }
+}
+
+function showResult(){
+
+}
+
+
+
+
